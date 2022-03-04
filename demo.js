dfb03e8b4a007e4f178c7a82983edccad4eb49c7
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-03-03 18:58:54 -0600

    Made demo

---
+++ demo.js
@@ -0,0 +1,11 @@
+function triangleArea(base, height){
+    return base * height * .5;
+}
+
+var area = triangleArea(5, 10);
+var area2 = triangleArea(2, 4);
+var area3 = triangleArea(4, 20);
+
+console.log(area);
+console.log(area2);
+console.log(area3);
