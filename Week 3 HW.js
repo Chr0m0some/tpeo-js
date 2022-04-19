6b1f17a7fc481a40603150fe4fd90dd86c1ae536
Chris <leework054@gmail.com>
2022-04-19 13:12:37 -0500

    Complete Week 3 HW

---
+++ Week 3 HW.js
@@ -0,0 +1,53 @@
+function countUpwardsByThree(){
+    var counter = 0;
+    for(var i = 0; i < 11; i++){
+        console.log(counter);
+        counter += 3;
+    }
+}
+
+function guessMyFavoriteNumber(guess){
+    var myFavoriteNumber = 14;
+    if(guess == myFavoriteNumber){
+        console.log("You guessed my favorite number!");
+    }
+    else{
+        console.log("Try again!");
+    }
+}
+
+function convertNumberToDay(number){
+    switch(number){
+        case 0:
+            console.log("Sunday");
+            break;
+        case 1:
+            console.log("Monday");
+            break;
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+    }
+}
+
+countUpwardsByThree();
+
+guessMyFavoriteNumber(12);
+guessMyFavoriteNumber(25);
+guessMyFavoriteNumber(14);
+
+for(var i = 0; i < 7; i++){
+    convertNumberToDay(i);
+}
